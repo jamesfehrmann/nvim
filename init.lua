52e8052ce8bdbeb5fc426f291aa70a105c227bf3
James Fehrmann <jfehrmann@outlook.com>
2023-12-15 10:05:15 -0500

    added git support

--- init.lua
+++ init.lua
@@ -9,5 +9,6 @@ require('lazy-setup')
 vim.cmd.colorscheme('catppuccin-mocha')
 --vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 --vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
-
+-- testing for git
+--
 
---
+++ lua/plugins/git.lua
@@ -0,0 +1,7 @@
+return {
+    'tpope/vim-fugitive',
+    'tpope/vim-rhubarb',
+    'tpope/vim-sleuth',
+    'tpope/vim-tbone',
+    { 'lewis6991/gitsigns.nvim', opts = {}},
+}
