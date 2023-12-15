5a1a2f7fea0581534005013560dc1a78c3c73603
James Fehrmann <jfehrmann@outlook.com>
2023-12-15 12:20:21 -0500

    test

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,7 +1,16 @@
 return {
-    'tpope/vim-fugitive',
+    {'tpope/vim-fugitive',
+    },
     'tpope/vim-rhubarb',
     'tpope/vim-sleuth',
     'tpope/vim-tbone',
-    { 'lewis6991/gitsigns.nvim', opts = {}},
+    { 'lewis6991/gitsigns.nvim', opts = {
+        signs = {
+            add = { text = '' },
+            change = { text = '' },
+            delete = { text = '󰞒' },
+            topdelete = { text = '󰞕' },
+            changedelete = { text = '~' },
+        },
+    }},
 }
