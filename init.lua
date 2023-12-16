6ebdd0618e357a91c048ab63ee43f6c8b36c51c6
James Fehrmann <jfehrmann@outlook.com>
2023-12-15 19:07:19 -0500

    cleanup

--- init.lua
+++ init.lua
@@ -1,6 +1,12 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+vim.api.nvim_set_keymap('n', '<F1>', '<NOP>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('v', '<F1>', '<NOP>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('s', '<F1>', '<NOP>', {noremap = true, silent = true})
+vim.api.nvim_set_keymap('o', '<F1>', '<NOP>', {noremap = true, silent = true})
+
+
 -- The home directory for these is ~/.config/nvim/lua
 require('keymaps')
 require('options')
@@ -9,6 +15,4 @@ require('lazy-setup')
 vim.cmd.colorscheme('catppuccin-mocha')
 --vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
 --vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
--- testing for git
---
 
--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,11 +1,14 @@
+-- https://github.com/neovim/nvim-lspconfig
+
 return {
-    {'neovim/nvim-lspconfig',
+    {
+        'neovim/nvim-lspconfig',
         event = { 'BufReadPre', 'BufNewFile' },
         dependencies = {
             'williamboman/mason.nvim',
             'williamboman/mason-lspconfig.nvim',
             'hrsh7th/cmp-nvim-lsp',
-            { 'j-hui/fidget.nvim', opts = {} },
+            { 'j-hui/fidget.nvim',                   opts = {} },
             'folke/neodev.nvim',
             { 'antosha417/nvim-lsp-file-operations', config = true },
         },
@@ -18,44 +21,28 @@ return {
             local on_attach = function(client, bufnr)
                 opts.buffer = bufnr
 
-                -- set keybinds
-                opts.desc = 'Show LSP references'
-                vim.keymap.set('n', 'gR', '<cmd>Telescope lsp_references<CR>', opts) 
-
-                opts.desc = 'Go to declaration'
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts) 
-
-                opts.desc = 'Show LSP definitions'
-                vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts) 
-
-                opts.desc = 'Show LSP implementations'
-                vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts) 
-
-                opts.desc = 'Show LSP type definitions'
-                vim.keymap.set('n', 'gt', '<cmd>Telescope lsp_type_definitions<CR>', opts)
-
-                opts.desc = 'See available code actions'
-                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-
-                opts.desc = 'Smart rename'
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-
-                opts.desc = 'Show buffer diagnostics'
-                vim.keymap.set('n', '<leader>D', '<cmd>Telescope diagnostics bufnr=0<CR>', opts)
-
-                opts.desc = 'Show line diagnostics'
-                vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, opts)
-
-                opts.desc = 'Go to previous diagnostic'
-                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-
-                opts.desc = 'Go to next diagnostic'
-                vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-
-                opts.desc = 'Show documentation for what is under cursor'
+                vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+                vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+                vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+                vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
                 vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts)
 
-                opts.desc = 'Restart LSP'
                 vim.keymap.set('n', '<leader>rs', ':LspRestart<CR>', opts)
             end
 
@@ -71,14 +58,12 @@ return {
             lspconfig['lua_ls'].setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
-                settings = { -- custom settings for lua
+                settings = {
                     Lua = {
-                        -- make the language server recognize 'vim' global
                         diagnostics = {
                             globals = { 'vim' },
                         },
                         workspace = {
-                            -- make language server aware of runtime files
                             library = {
                                 [vim.fn.expand('$VIMRUNTIME/lua')] = true,
                                 [vim.fn.stdpath('config') .. '/lua'] = true,
@@ -100,13 +85,11 @@ return {
                 on_attach = on_attach,
             })
 
-            -- configure python server
+            -- configure clangd server
             lspconfig['clangd'].setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
             })
-
-
         end,
     }
 
